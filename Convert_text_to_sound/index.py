75e10ea28c14c3382ab1e9811589652a6cf1a2e9
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2022-10-05 22:05:34 +0300

    Update index.py

--- Convert_text_to_sound/index.py
+++ Convert_text_to_sound/index.py
@@ -1,12 +1,11 @@
 from gtts import gTTS                                                         
 from playsound import playsound                                               
 #اكتب النص المراد تحويلة  الى صوت هنا                                                 
-mytext="welcome my student in artifical intelligence  how are you ?" \        
-       "you are best student "                                                
+mytext="Hello everyone this Welcom in My program "                                        
 language='en'                                                                 
 myobj=gTTS(text=mytext,lang=language,slow=True)
 #سيتم حفظ الصوت في الملف الي في البرنامج
-myobj.save("welcome1.mp3")                                                    
-playsound("welcome1.mp3")                                                     
+myobj.save("sound.mp3")                                                    
+playsound("sound.mp3")                                                     
                                                                               
                                                                               
