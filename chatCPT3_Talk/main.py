f225cf4fdb12dd306429abf1786275f0d950c731
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2023-05-12 22:42:02 +0300

    Create main.py

---
+++ chatCPT3_Talk/main.py
@@ -0,0 +1,5 @@
+from chatclass import chat_talk
+api="" #Write the api of chatgpt here
+chat=chat_talk('en',api) #here ypo can choos  languag ar(arabic) or en 
+h=chat.start()
+print(h)#will retern the respons from chat
