aa424972e127a5d7ae43a0d2a811e50376aee30f
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2023-07-13 22:48:32 +0300

    Update train_main.py

--- PDF_Asking/train/train_main.py
+++ PDF_Asking/train/train_main.py
@@ -1,9 +1,9 @@
 import os
 import streamlit as st
 
-from Data_train import My_own_gpt
+from Data_train import My_own_gpt  # import the My_own_gpt function from the Data_train module
 
-models = [
+models = [  # create a list of available GPT models
     "gpt-3.5-turbo-16k-0613",
     "gpt-3.5-turbo",
     "text-davinci-003",
@@ -14,26 +14,27 @@ models = [
     "text-davinci-001",
     "ZeyadAhmed/AraElectra-Arabic-SQuADv2-QA",
 ]
-# API="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"
-API = "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"
-api_key = "eb457224-95f8-4881-a32d-b28bcf8adb23"
-env = "us-west4-gcp-free"
-index_name = "chat"
 
-
-
+# set the API key and other parameters for accessing the OpenAI API
+API = ""
+api_key = ""
+env = ""
+index_name = ""
 
 def main():
+    # set the page title and display a header
     st.set_page_config(page_title="تدريب بياناتك  📝📗")
     st.header("ادخل البيانات  📝📗")
+    
+    # allow users to upload PDF documents
     pdf_docs = st.file_uploader(
         "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    
+    # when the "Process" button is pressed, train the GPT model on the uploaded PDFs
     if st.button("Process"):
         with st.spinner("Processing"):
             My_own_gpt(pdf_docs, models[0], API, api_key, env, index_name)
             st.text(" 🎩🏆تم التدريب بنجاح")
 
-
-
 if __name__ == "__main__":
-    main()
+    main()  # call the main function when the script is executed
